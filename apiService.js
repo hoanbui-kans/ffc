6d7f98da18297e8689c992d4458756b91fad7088
Hoan Bui (Tony) <hoanbui.kans@gmail.com>
2025-03-11 23:02:25 +0700

    Update

---
+++ apiService.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+const client = require('ssi-fcdata');
+const config = require('./config.js');
+
+const apiClient = axios.create({
+  baseURL: config.market.ApiUrl,
+  timeout: 5000,
+});
+
+// Hàm xác thực và khởi tạo WebSocket
+async function authenticate() {
+  try {
+    const response = await apiClient.post(client.api.GET_ACCESS_TOKEN, {
+      consumerID: config.market.ConsumerId,
+      consumerSecret: config.market.ConsumerSecret,
+    });
+
+    if (response.data.status === 200) {
+      const token = "Bearer " + response.data.data.accessToken;
+
+      // Gán token cho mọi request sau này
+      apiClient.interceptors.request.use((config) => {
+        config.headers.Authorization = token;
+        return config;
+      });
+
+      // Khởi tạo WebSocket
+      client.initStream({ url: config.market.HubUrl, token });
+      client.bind(client.events.onData, (data) => { console.log("data:", data)});
+      // client.bind(client.events.onConnected, () => client.switchChannel("X-QUOTE:ALL"));
+      client.start();
+    } else {
+      console.error("Authentication Failed:", response.data.message);
+    }
+  } catch (error) {
+    console.error("Auth Error:", error.message);
+  }
+}
+
+// Hàm gửi request chung
+async function fetchData(apiEndpoint, queryParams, res) {
+  try {
+    const response = await apiClient.get(apiEndpoint, { params: queryParams });
+    res.json(response.data);
+  } catch (error) {
+    console.error("API Error:", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+module.exports = { fetchData, authenticate };
--- config.js
+++ config.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 module.exports = {  
 	market: {
 		HubUrl: "wss://fc-datahub.ssi.com.vn/",
 		ApiUrl: "https://fc-data.ssi.com.vn/",
-		ConsumerId: "1b8fb14a6f384339a6976d90a007aa7b",
-		ConsumerSecret: "78c989f9e52d4df3ae07337ca1bd8bcd"
+		ConsumerId: process.env.CONSUMER_ID,
+		ConsumerSecret: process.env.CONSUMER_SECRET
 	},
 };
