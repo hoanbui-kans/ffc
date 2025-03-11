b9f83eea1e03bf7e5fed59397920bd70cee390c1
Hoan Bui (Tony) <hoanbui.kans@gmail.com>
2025-03-11 21:22:37 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {  
+	market: {
+		HubUrl: "wss://fc-datahub.ssi.com.vn/",
+		ApiUrl: "https://fc-data.ssi.com.vn/",
+		ConsumerId: "1b8fb14a6f384339a6976d90a007aa7b",
+		ConsumerSecret: "78c989f9e52d4df3ae07337ca1bd8bcd"
+	},
+};
